3418b8cafddb3b2bd963df56331741dfbf9cc43e
by151709129 <mg86837242@protonmail.com>
2023-03-23 14:18:02 +1000

    tweak commments

--- src/components/UI/SelectSmallGroupGender.jsx
+++ src/components/UI/SelectSmallGroupGender.jsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from 'react';
+
 export default function SelectSmallGroupGender({ issue, defaultValue, isRequired }) {
+  const [latestDefault, setLatestDefault] = useState('');
+
+  useEffect(() => {
+    let ignore = false;
+    if (!ignore) {
+      setLatestDefault(defaultValue);
+    }
+    return () => {
+      ignore = true;
+    };
+  }, [defaultValue]);
+
+  // console.log(`🟢 [${new Date().toLocaleTimeString()}] latestDefault: ${latestDefault}`);
+  // ??? Default value doesn't work as intended, currently print "Male", but not reflected in the default selected option (also print `defaultValues` fires 4 times in `ProfilePanel` component)
+
   return (
     <div id='gender-select-group' className='w-full form-control'>
       <label htmlFor='gender' className='py-1 3xl:py-2 label'>
@@ -8,8 +25,7 @@ export default function SelectSmallGroupGender({ issue, defaultValue, isRequired
       <select
         name='gender'
         id='gender'
-        // FIX Make default value work, currently print "Male", but not reflected in the default selected option
-        defaultValue={defaultValue}
+        defaultValue={latestDefault}
         className='min-h-0 font-normal select select-primary select-sm'
       >
         {/* NB If this option is disabled and its value (empty string) is used as `defaultValue`, the `<select>`'s value will be null in the formData, i.e. DON'T disable this option if its value is used as `defaultValue`*/}
