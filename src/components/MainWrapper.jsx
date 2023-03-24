5ff5a8eeb10e2c5ee85741f7486eed886ec2b302
by151709129 <mg86837242@protonmail.com>
2023-03-24 15:47:14 +1000

    fix main ele height bug

--- src/components/MainWrapper.jsx
+++ src/components/MainWrapper.jsx
@@ -4,11 +4,9 @@ export default function MainWrapper({ children, gap, mt, mb }) {
   return (
     <main
       id='main-element-wrapper'
-      className='grid items-center w-full h-full grid-cols-1 grid-rows-1 justify-items-center'
+      className='grid items-center w-full min-h-[80vh] grid-cols-1 grid-rows-1 justify-items-center'
     >
-      <div
-        className={`grid items-start w-full h-full justify-items-center min-h-[80vh] ${gap} ${mt} ${mb} col-[1_/_2] row-[1_/_2]`}
-      >
+      <div className={`grid items-start w-full h-full justify-items-center ${gap} ${mt} ${mb} col-[1_/_2] row-[1_/_2]`}>
         {children}
       </div>
       <Loading className={'sticky flex items-start justify-center w-full h-full col-[1_/_2] row-[1_/_2]'} />
