4986e988c485911a0f08e9f565599765f6fc610d
by151709129 <mg86837242@protonmail.com>
2023-03-25 21:00:20 +1000

    chkpt

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,12 +1,8 @@
 import MainWrapper from '../components/MainWrapper';
 import UnderConstruction from '../components/UnderConstruction';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
-
 export default function Blogs() {
   return (
     <MainWrapper gap={'gap-16'} mt={'mt-16'} mb={'mb-20'}>
-      <Skeleton />
       <UnderConstruction
         pageName={'blogs'}
         imageUrl={
