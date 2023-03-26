a448b9a5d89deadf6679c040834a49a0746243b8
by151709129 <mg86837242@protonmail.com>
2023-03-26 10:22:49 +1000

    dropdown filter and format

--- src/components/Bookings/BookingList.jsx
+++ src/components/Bookings/BookingList.jsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import AuthContext from '../../contexts/AuthContext';
 import { useLoaderData, NavLink, Outlet } from 'react-router-dom';
 import { monthNames, getOrdinal } from '../../utils/mapDates';
@@ -32,8 +32,9 @@ export default function BookingList() {
 }
 
 function BookingListMemberView({ bookings, userMemberId }) {
-  // TODO (1) my booking list view (dropdown), with all list having the link to details, (2) my booking list view (dropdown) for trainer, but not for admin
-  const bookingList = useMemo(
+  // TODO (1) no booking prompt if nothing in myBookingList, (2) my booking list view (dropdown) for trainer, but not for admin
+  const [selectedValue, setSelectedValue] = useState('all-booking-list');
+  const allBookingList = useMemo(
     () =>
       bookings.map(
         (
@@ -114,6 +115,63 @@ function BookingListMemberView({ bookings, userMemberId }) {
       ),
     [bookings, userMemberId]
   );
+  const myBookingList = useMemo(
+    () =>
+      bookings.map(
+        (
+          {
+            id,
+            memberId,
+            memberFirstName,
+            memberLastName,
+            trainerFirstName,
+            trainerLastName,
+            activityName,
+            dateTime,
+            durationMinutes,
+          },
+          i
+        ) =>
+          userMemberId === memberId && (
+            <li id='booking-list-card' key={i} className='w-full rounded-lg bg-base-300 max-w-[22rem]'>
+              <NavLink
+                to={`id/${id}`}
+                className={({ isActive, isPending }) =>
+                  `grid grid-cols-2 px-4 xl:px-6 py-2 ${isActive && 'ring ring-secondary-focus rounded-lg'} ${
+                    isPending && ''
+                  }`
+                }
+              >
+                <p className='p-1'>Booking ID:</p>
+                <p className='p-1'>{id}</p>
+                <p className='p-1 bg-base-100'>Member:</p>
+                <p className='p-1 bg-base-100'>
+                  {memberFirstName} {memberLastName}
+                </p>
+                <p className='p-1'>Trainer:</p>
+                <p className='p-1'>
+                  {trainerFirstName} {trainerLastName}
+                </p>
+                <p className='p-1 bg-base-100'>Activity:</p>
+                <p className='p-1 bg-base-100'>{activityName}</p>
+                <p className='p-1'>Date:</p>
+                <p className='p-1'>
+                  {dateTime.slice(8, 9) === '0' ? dateTime.slice(9, 10) : dateTime.slice(8, 10)}
+                  {getOrdinal(dateTime.slice(8, 10))} {monthNames[Number(dateTime.slice(5, 7))]} {dateTime.slice(0, 4)}
+                </p>
+                <p className='p-1 bg-base-100'>Time:</p>
+                <p className='p-1 bg-base-100'>{dateTime.slice(-8)}</p>
+                <p className='p-1'>Duration:</p>
+                <p className='p-1'>{durationMinutes} minutes</p>
+              </NavLink>
+            </li>
+          )
+      ),
+    [bookings, userMemberId]
+  );
+  console.log('🟢');
+  console.log(myBookingList);
+  console.log('🟢');
 
   return (
     <>
@@ -121,11 +179,17 @@ function BookingListMemberView({ bookings, userMemberId }) {
         id='booking-list-wrapper'
         className='flex flex-col items-center gap-5 w-full col-[1_/_2] row-[2_/_3] lg:col-[2_/_3] lg:row-[1_/_2]'
       >
-        <select className='select select-primary select-sm w-full max-w-xs'>
-          <option>All bookings</option>
-          <option>My bookings</option>
+        <select
+          value={selectedValue}
+          onChange={(e) => setSelectedValue(e.target.value)}
+          className='select select-primary select-sm w-full max-w-xs'
+        >
+          <option value='all-booking-list'>All bookings</option>
+          <option value='my-booking-list'>My bookings</option>
         </select>
-        <ul className='grid justify-items-center content-start w-full gap-4'>{bookingList}</ul>
+        <ul className='grid justify-items-center content-start w-full gap-4'>
+          {selectedValue === 'all-booking-list' ? allBookingList : myBookingList}
+        </ul>
       </div>
       <Outlet />
     </>
