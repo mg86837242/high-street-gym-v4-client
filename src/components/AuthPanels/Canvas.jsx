e2ce91df297ef0696d05682fa118d5b97f3d0b48
by151709129 <mg86837242@protonmail.com>
2023-03-28 02:25:18 +1000

    fix link

--- src/components/AuthPanels/Canvas.jsx
+++ src/components/AuthPanels/Canvas.jsx
@@ -2,8 +2,7 @@ export default function Canvas({ children }) {
   return (
     <div
       id='canvas'
-      className={`flex flex-col items-center w-full h-full pb-8 md:pb-0 bg-base-300 bg-[url('src/assets/home-hero.webp')] bg-cover bg-top`}
-      // ??? Frequently fail to load as background image
+      className={`flex flex-col items-center w-full h-full pb-8 md:pb-0 bg-base-300 bg-[url('/src/assets/home-hero.webp')] bg-cover bg-top`}
     >
       {children}
     </div>
