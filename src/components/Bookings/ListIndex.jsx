f5475c655342f0bb7eddd99412ef04aef570bd46
by151709129 <mg86837242@protonmail.com>
2023-04-01 17:30:49 +1000

    tweak

--- src/components/Bookings/ListIndex.jsx
+++ src/components/Bookings/ListIndex.jsx
@@ -7,7 +7,7 @@ export default function ListIndex() {
       className='lg:grid lg:place-items-center col-[1_/_2] row-[2_/_3] lg:col-[2_/_3] lg:row-[1_/_2] lg:min-h-[80vh] lg:sticky lg:top-28'
     >
       <p className='text-lg text-center'>
-        👈 Click a date to see a list of bookings, if any, on that date (for example, 7th Mar 2023), or{' '}
+        👈 Click a date to see a list of bookings, if any, on that date (for example, 28th Apr 2023), or{' '}
         <Link to='new' className='link link-primary'>
           create a new booking
         </Link>
