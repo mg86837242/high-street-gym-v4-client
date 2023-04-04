906efea1e694077dd05b7964743418b9ebfb767f
by151709129 <mg86837242@protonmail.com>
2023-04-04 13:43:36 +1000

    union type cleanup

--- src/schemas/trainers.js
+++ src/schemas/trainers.js
@@ -3,16 +3,7 @@ import { z } from 'zod';
 export const descriptionSchema = z.string().max(255).nullable();
 export const specialtySchema = z.string().max(45).nullable();
 export const certificateSchema = z.string().max(45).nullable();
-export const imageUrlSchema = z
-  .string()
-  .refine(
-    (val) =>
-      val === '' ||
-      (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/.test(
-        val
-      ) === true &&
-        val.length <= 255),
-    {
-      message: 'Invalid url format',
-    }
-  );
+export const imageUrlSchema = z.union([
+  z.string().length(0, { message: 'Image url must be empty or a valid url' }),
+  z.string().url(),
+]);
