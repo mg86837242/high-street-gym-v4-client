3146b3270ab471ba7ac494180eafddfd61cef00b
by151709129 <mg86837242@protonmail.com>
2023-04-05 14:12:08 +1000

    format & lint

--- src/utils/getDateNotation.js
+++
@@ -1,5 +0,0 @@
-export default function getDateNotation(dateTime) {
-  return `${dateTime.slice(8, 9) === '0' ? dateTime.slice(9, 10) : dateTime.slice(8, 10)}${getOrdinal(
-    dateTime.slice(8, 10)
-  )} ${monthNames[Number(dateTime.slice(5, 7))]} ${dateTime.slice(0, 4)}`;
-}
--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -1,7 +1,6 @@
 export default function sanitize(obj) {
-  const sanitizedObj = Object.keys(obj).reduce(
-    (acc, cur) => (obj[cur] === '' ? (acc[cur] = null) : (acc[cur] = obj[cur]), acc),
-    {}
-  );
-  return sanitizedObj;
+  return Object.keys(obj).reduce((acc, cur) => {
+    obj[cur] === '' ? (acc[cur] = null) : (acc[cur] = obj[cur]);
+    return acc;
+  }, {});
 }
