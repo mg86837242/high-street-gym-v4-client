55b6a0621bd32c1717ed6fd5aa5e5d326f22bd7a
by151709129 <mg86837242@protonmail.com>
2023-04-11 03:39:24 +1000

    tweak helpers

--- src/helpers/getSubmittedData.js
+++ src/helpers/getSubmittedData.js
@@ -19,17 +19,14 @@ export default async function getSubmittedData(request) {
 
 // References:
 // -- https://github.com/remix-run/remix/discussions/3680 (source: google "remix action request json")
-// -- https://github.com/react-hook-form/react-hook-form/issues/656#issuecomment-680674438: strip out all values with
-//  empty strings with lodash util
-// -- https://stackoverflow.com/questions/22783108/convert-js-object-to-form-data (source: google "lodash convert
-//  specific object values") => from which how to only convert values with empty strings within an obj is extrapolated
 
-// NB Alternatively, submit a `FormData` obj to the route action, which implies conversion from plain obj
+// Alternatively, submit a `FormData` obj to the route action, which implies conversion from plain obj
 //  (`data` returned by React Hook Form's `handleSubmit`) to `FormData` obj, which entails a util like this:
 //  https://stackoverflow.com/questions/22783108/convert-js-object-to-form-data; this method has a huge
 //  downside since `FormData` can only hold strings in most cases, which implies extra type conversions from
 //  empty string to null in the route action in order to bypass the db constraint (i.e., db only accepts null)
 //
-// Example in `<AdminEditActivity>` component:
-// const formData = getFormData(data);
-// submit(formData, { method: 'post' });
+// Example in <EditActivity> component (see: https://stackoverflow.com/questions/22783108/ (source: google
+//  "lodash convert specific object values" for `getFormData()` util)
+//   const formData = getFormData(data);
+//   submit(formData, { method: 'post' });
