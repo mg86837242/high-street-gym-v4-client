f088bdc81deb31f8154e3cbb759e78a182328a57
by151709129 <mg86837242@protonmail.com>
2023-04-14 22:35:12 +1000

    cleanup schema

--- src/schemas/members.js
+++ src/schemas/members.js
@@ -33,3 +33,10 @@ export const memberSchema = z.object({
   id: z.number(),
   _action: z.string(),
 });
+
+// NB Empty string within the `enum()` is for the "Choose ..." option in case the user wants to intentionally leave it
+//  blank
+
+// NB Empty input of "type='number" will be deemed as NaN during the validation by React Hook Form (or resolver), thus
+//  this union type (however, will be converted to empty string after being returned from `handleSubmit()`), see this
+//  Github post for alternative solution: https://github.com/orgs/react-hook-form/discussions/6980
