56aefaef6b49675dc966d296ca2e9c87e845192f
by151709129 <mg86837242@protonmail.com>
2023-04-15 16:43:54 +1000

    cleanup schema

--- src/schemas/addresses.js
+++ src/schemas/addresses.js
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const lineOneSchema = z.string().max(45);
 export const lineTwoSchema = z.string().max(45);
 export const suburbSchema = z.string().max(45);
-export const postcodeSchema = z.string().max(10);
+export const postcodeSchema = z.string().max(45);
 export const stateSchema = z.string().max(45);
 export const countrySchema = z.string().max(45);
 
