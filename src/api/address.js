d25f79460b34f5db670fb5c768eb9f8ac749b991
by151709129 <mg86837242@protonmail.com>
2023-04-19 14:05:18 +1000

    bugfix

--- src/api/address.js
+++ src/api/address.js
@@ -2,19 +2,19 @@ import { API_URL } from '../data/constants';
 import fetchJSON from '../helpers/fetchJSON';
 
 export async function updateAddressByAdminId(values) {
-  const { adminId, ...updates } = values;
+  const { _action, adminId, ...updates } = values;
   const json = await fetchJSON.patch(`${API_URL}/addresses/by/admin_id/${adminId}`, updates);
-  return { ...json, _action: 'updateAddressByAdminId' };
+  return { ...json, _action };
 }
 
 export async function updateAddressByTrainerId(values) {
-  const { trainerId, ...updates } = values;
+  const { _action, trainerId, ...updates } = values;
   const json = await fetchJSON.patch(`${API_URL}/addresses/by/trainer_id/${trainerId}`, updates);
-  return { ...json, _action: 'updateAddressByTrainerId' };
+  return { ...json, _action };
 }
 
 export async function updateAddressByMemberId(values) {
-  const { memberId, ...updates } = values;
+  const { _action, memberId, ...updates } = values;
   const json = await fetchJSON.patch(`${API_URL}/addresses/by/member_id/${memberId}`, updates);
-  return { ...json, _action: 'updateAddressByMemberId' };
+  return { ...json, _action };
 }
