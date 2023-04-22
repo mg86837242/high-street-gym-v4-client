bdbeded4cd6955cabb2d0258b4bb0b3aaddc472c
by151709129 <mg86837242@protonmail.com>
2023-04-22 15:33:49 +1000

    adjust notes

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -76,6 +76,7 @@ export default function AuthProvider({ children }) {
   );
 
   const value = useMemo(
+    // It would be cached as an interface in TS, so the equivalent in JS is just a plain obj
     () => ({
       user,
       handleLogin,
