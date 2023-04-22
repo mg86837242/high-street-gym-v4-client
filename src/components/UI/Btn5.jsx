7d52546f266dface7288042b442a9be902278618
by151709129 <mg86837242@protonmail.com>
2023-04-22 13:41:00 +1000

    fix ui
    btn 5 text contrast

--- src/components/UI/Btn5.jsx
+++ src/components/UI/Btn5.jsx
@@ -1,6 +1,10 @@
 export function Btn5({ children, type, onClick, w }) {
   return (
-    <button type={type} onClick={onClick} className={`${w} shadow btn glass bg-base-100 shadow-black/50`}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${w} shadow btn glass bg-base-100 text-primary-content shadow-black/50`}
+    >
       {children}
     </button>
   );
@@ -8,7 +12,11 @@ export function Btn5({ children, type, onClick, w }) {
 
 export function Btn5Sm({ children, type, onClick, w }) {
   return (
-    <button type={type} onClick={onClick} className={`${w} shadow btn glass bg-base-100 btn-sm shadow-black/50`}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${w} shadow btn glass bg-base-100 btn-sm text-primary-content shadow-black/50`}
+    >
       {children}
     </button>
   );
