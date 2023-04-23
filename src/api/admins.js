656e4710cac236c21e5e60503fad3c4adf9d77f5
by151709129 <mg86837242@protonmail.com>
2023-04-23 10:01:16 +1000

    cleanup

--- src/api/admins.js
+++ src/api/admins.js
@@ -1,4 +1,3 @@
-import { redirect } from 'react-router-dom';
 import { API_URL } from '../data/constants';
 import fetchRaw from '../helpers/fetchRaw';
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,13 @@ import './index.css';
 import '@fontsource/inter';
 import '@fontsource/righteous';
 import '@fontsource/permanent-marker';
-import { isProd } from './data/constants';
 import AuthProvider from './components/AuthProvider';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  isProd ? (
+  <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  ) : (
-    <React.StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </React.StrictMode>
-  )
+  </React.StrictMode>
 );
