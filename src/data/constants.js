4977981e82e5492e2400dc8437a43f54d05cf77c
by151709129 <mg86837242@protonmail.com>
2023-07-10 03:57:35 +1000

    up comments

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,4 +2,9 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 export default API_URL;
 // References:
+// -- https://vitejs.dev/guide/env-and-mode.html#env-variables
 // -- https://vitejs.dev/guide/env-and-mode.html#env-files
+// -- https://vitejs.dev/config/#using-environment-variables-in-config
+// -- https://vitejs.dev/config/shared-options.html#envprefix
+// -- https://vitejs.dev/config/shared-options.html#define
+// -- https://dev.to/whchi/how-to-use-processenv-in-vite-ho9
