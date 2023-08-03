b768779feca76c94d762fdc2985f27bcf5612e09
by151709129 <mg86837242@protonmail.com>
2023-08-04 00:09:22 +1000

    ch css spinner class name to not conflict with daisyui

--- src/components/UI/Spinner.jsx
+++ src/components/UI/Spinner.jsx
@@ -5,7 +5,7 @@ export default function Spinner() {
 
   return (
     <div className={navigation.state === 'idle' ? 'hidden' : 'fixed bottom-0 left-0 right-0 top-0'}>
-      <div className='loading-spinner absolute top-1/2 mt-[-43px]'>
+      <div className='sliding-squares absolute top-1/2 mt-[-43px]'>
         <div id='square1'></div>
         <div id='square2'></div>
         <div id='square3'></div>
@@ -15,3 +15,7 @@ export default function Spinner() {
     </div>
   );
 }
+
+// References:
+// -- https://codepen.io/tag/sliding-squares?cursor=ZD0xJm89MCZwPTE=
+// -- https://stackoverflow.com/questions/45991160/css-spinner-as-full-page-overlay
